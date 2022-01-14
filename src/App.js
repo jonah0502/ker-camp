bd850b79331a0290214eb06b1564339ff8c48294
jonah0502 <jonah0502@gmail.com>
2022-01-13 17:59:04 -0800

    working contact page navbarr

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ function App() {
     <>
     <Header/>
   <div>
-    <h1>Campaign</h1>
+  <h1>Kindle Greatness with Kerner and Griffiths!</h1>
+
+<h3>Have you ever wondered who amends student fees, represents student voices to the OSU administration, and ultimately makes valuable changes on campus for the student body? To answer your question, it is the student government of Oregon State University, also known as the Associated Students of Oregon State University (ASOSU). Many may be unaware of the positive changes that ASOSU can make, so it is a goal of Alexander Kerner and Isabella Griffiths to involve the student body in the amazing opportunities that ASOSU can provide. The Kerner and Griffiths administration has the intent for ASOSU to partner and collaborate directly with the student body so that we are able to thoroughly represent each individual on campus. We want to hear your concerns and ideas to ultimately work together to make OSU 4 YOU!</h3>
+
+<h3>OSU 4 YOU is a campaign that was created to improve YOUR student services and experiences. We plan on:</h3>
+
   </div>
   </>
   );
